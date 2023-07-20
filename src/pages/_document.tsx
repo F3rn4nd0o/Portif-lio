0342f147e1b9817e71b4f61e01db89ad3f260d44
F3rn4nd0o <fernandovasconcelosmelo@outlook.com>
2023-07-20 00:56:47 -0300

    Add Favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,27 @@
-import { Html, Head, Main, NextScript} from 'next/document'
-import { getCssText } from '../styles'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { getCssText } from '../styles';
 
 export default function Document() {
-  return(
+  return (
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ccircle cx='50' cy='50' r='48' fill='black' stroke='white' stroke-width='4' /%3E%3Ctext x='50' y='52' font-size='56' text-anchor='middle' alignment-baseline='middle' fill='white'%3EF%3C/text%3E%3C/svg%3E"
+        />
+        <title>Fernando V. Melo Costa</title>
       </Head>
       <body>
         <Main />
-        <NextScript/>
+        <NextScript />
       </body>
     </Html>
-  )
+  );
 }
